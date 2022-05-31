6a54d4940e708e9c84384ffa5eff3855db016ed7
Nick Wilson <7313396+TheNickWilson@users.noreply.github.com>
2022-05-31 13:35:12 +0100

    Configure journey

---
+++ src/test/scala/uk/gov/hmrc/perftests/example/Requests.scala
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.perftests.example
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.http.request.builder.HttpRequestBuilder
+import uk.gov.hmrc.performance.conf.ServicesConfiguration
+
+object Requests extends ServicesConfiguration {
+
+  val baseUrl: String = baseUrlFor("national-insurance-uplift-calculator-frontend")
+  val route: String   = "/estimate-how-national-insurance-contributions-changes-affect-you"
+
+  val navigateToHomePage: HttpRequestBuilder =
+    http("Navigate to Home Page")
+      .get(s"$baseUrl$route/")
+      .check(status.is(200))
+
+  val getEmploymentStatusPage: HttpRequestBuilder =
+    http("Get Employment Status Page")
+      .get(s"$baseUrl$route/employment-status")
+      .check(status.is(200))
+      .check(css("input[name=csrfToken]", "value").saveAs("csrfToken"))
+
+  val postEmploymentStatusPage: HttpRequestBuilder =
+    http("Post Employment Status Page")
+      .post(s"$baseUrl$route/employment-status")
+      .formParam("value", "employed")
+      .formParam("csrfToken", s"$${csrfToken}")
+      .check(status.is(303))
+      .check(header("Location").is(s"$route/annual-salary").saveAs("annualSalaryPage"))
+
+  val getAnnualSalaryPage: HttpRequestBuilder =
+    http("Get Annual Salary Page")
+      .get(s"$baseUrl$${annualSalaryPage}")
+      .check(status.is(200))
+      .check(css("input[name=csrfToken]", "value").saveAs("csrfToken"))
+
+  val postAnnualSalaryPage: HttpRequestBuilder =
+    http("Post Annual Salary Page")
+      .post(s"$baseUrl$${annualSalaryPage}")
+      .formParam("value", "25000")
+      .formParam("csrfToken", s"$${csrfToken}")
+      .check(status.is(303))
+      .check(header("Location").is(s"$route/estimated-change").saveAs("resultPage"))
+
+  val getResultPage: HttpRequestBuilder =
+    http("Get Result Page")
+      .get(s"$baseUrl$${resultPage}")
+      .check(status.is(200))
+}
